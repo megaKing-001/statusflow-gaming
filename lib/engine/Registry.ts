ffae9209f9ddf7ef94374cf5230fb0ff77db68fb
megaKing-001 <statusflow86@gmail.com>
2026-09-10 19:38:16 +0100

    Add StatusWheelEngine to the registry

--- lib/engine/Registry.ts
+++ lib/engine/Registry.ts
@@ -1,10 +1,12 @@
 import { StatusQuizEngine } from './games/StatusQuizEngine';
 import { StatusCrashEngine } from './games/StatusCrashEngine';
+import { StatusWheelEngine } from './games/StatusWheelEngine';
 
 const registry = new Map<string, any>();
 
 registry.set('statusquiz', new StatusQuizEngine());
 registry.set('statuscrash', new StatusCrashEngine());
+registry.set('statuswheel', new StatusWheelEngine());
 
 export function getGameEngine(slug: string) {
   const engine = registry.get(slug);
